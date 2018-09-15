a78e05ee79881b4de287908b04c9b0e7538f7fbf
Michelle <wmichelle123@gmail.com>
2018-09-15 09:24:26 -0400

    setup firebase

--- app/src/main/java/com/example/michellewong/fitbeat/MainActivity.java
+++ app/src/main/java/com/example/michellewong/fitbeat/MainActivity.java
@@ -4,6 +4,11 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 import com.spotify.android.appremote.api.ConnectionParams;
 import com.spotify.android.appremote.api.Connector;
 import com.spotify.android.appremote.api.SpotifyAppRemote;
@@ -17,6 +22,8 @@ public class MainActivity extends AppCompatActivity {
     private static final String CLIENT_ID = "2e6beef170124eccbc991779c72830f6";
     private static final String REDIRECT_URI = "fitbeat://callback";
     private SpotifyAppRemote mSpotifyAppRemote;
+    private static final String TAG = "MainActivity";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -54,6 +61,25 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void connected() {
+
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference myRef = database.getReference("message");
+        myRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                // This method is called once with the initial value and again
+                // whenever data at this location is updated.
+                String value = dataSnapshot.getValue(String.class);
+                Log.d(TAG, "Value is: " + value);
+            }
+
+            @Override
+            public void onCancelled(DatabaseError error) {
+                // Failed to read value
+                Log.w(TAG, "Failed to read value.", error.toException());
+            }
+        });
+
         mSpotifyAppRemote.getPlayerApi().play("spotify:user:spotify:playlist:37i9dQZF1DX2sUQwD7tbmL");    }
 
     @Override
