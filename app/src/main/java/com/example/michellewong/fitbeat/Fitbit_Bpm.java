15ba374305aac4594dfb7cbdbeced3d7defd1954
Jenny <jennyw12321@gmail.com>
2018-09-15 19:52:45 -0400

    asdf

---
+++ app/src/main/java/com/example/michellewong/fitbeat/Fitbit_Bpm.java
@@ -0,0 +1,19 @@
+package com.example.michellewong.fitbeat;
+
+/**
+ * Created by jennywang on 2018-09-15.
+ */
+
+public class Fitbit_Bpm {
+
+    private String bpm;
+
+    public Fitbit_Bpm(String bpm) {
+        this.bpm = bpm;
+    }
+
+    public String getBpm() {
+        return bpm;
+    }
+
+}
--- app/src/main/java/com/example/michellewong/fitbeat/MainActivity.java
+++ app/src/main/java/com/example/michellewong/fitbeat/MainActivity.java
@@ -1,13 +1,17 @@
 package com.example.michellewong.fitbeat;
 
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 
+import com.google.firebase.database.ChildEventListener;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
 import com.google.firebase.database.ValueEventListener;
 import com.spotify.android.appremote.api.ConnectionParams;
 import com.spotify.android.appremote.api.Connector;
@@ -23,6 +27,8 @@ public class MainActivity extends AppCompatActivity {
     private static final String REDIRECT_URI = "fitbeat://callback";
     private SpotifyAppRemote mSpotifyAppRemote;
     private static final String TAG = "MainActivity";
+
+    private DatabaseReference db;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -62,21 +68,20 @@ public class MainActivity extends AppCompatActivity {
 
     private void connected() {
 
-        FirebaseDatabase database = FirebaseDatabase.getInstance();
-        DatabaseReference myRef = database.getReference("message");
-        myRef.addValueEventListener(new ValueEventListener() {
+        db = FirebaseDatabase.getInstance().getReference();
+        String bpm = db.push().getKey();
+
+        db.child(bpm).addValueEventListener(new ValueEventListener() {
             @Override
-            public void onDataChange(DataSnapshot dataSnapshot) {
-                // This method is called once with the initial value and again
-                // whenever data at this location is updated.
-                String value = dataSnapshot.getValue(String.class);
-                Log.d(TAG, "Value is: " + value);
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                Fitbit_Bpm fb_bpm = dataSnapshot.getValue(Fitbit_Bpm.class);
+
+                Log.d("GOT BPM OF: %s", fb_bpm.getBpm());
             }
 
             @Override
-            public void onCancelled(DatabaseError error) {
-                // Failed to read value
-                Log.w(TAG, "Failed to read value.", error.toException());
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+                Log.w(TAG, "Failed to read value.", databaseError.toException());
             }
         });
 
